468d755e2f5931e4a14653408fab814d5b34bd61
ernesto <olaez_ernesto@hotmail.com>
2019-04-24 20:44:13 -0500

    ok

---
+++ HowProgramInJava/Unit2/Welcome1/Welcome.java
@@ -0,0 +1,12 @@
+package Welcome1;
+
+public class Welcome {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the JAVA Programming!");
+
+	}
+
+}
