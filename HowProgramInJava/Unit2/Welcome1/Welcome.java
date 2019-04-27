6206f88292cfdc0639894f56f71aea487ce4a858
ernesto <olaez_ernesto@hotmail.com>
2019-04-27 08:21:09 -0500

    Add new line
    Add new line for atom description

--- HowProgramInJava/Unit2/Welcome1/Welcome.java
+++ HowProgramInJava/Unit2/Welcome1/Welcome.java
@@ -2,11 +2,12 @@ package Welcome1;
 
 public class Welcome {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the JAVA Programming!");
 		System.out.println("Ernesto is the new one in the software");
+		System.out.println("This is my first edition in ATOM");
 
 	}
 
